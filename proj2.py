c9a2703ab3012722bbe5e56929cac7c60186fa23
Charles Tetteh-Quaye <ctquaye@gmail.com>
2020-02-06 22:43:20 -0600

    about to handle probs

--- proj2.py
+++ proj2.py
@@ -1,40 +1,64 @@
 #extract Data Function
 
-def readData(fl):
+def processGrams(fl):
     trainData={}
-    unigramList={}
-    bigramTuple=()
-    trigramList=[]
-    totalCount=0
+    unigramDict={}
+    bigramDict={}
+    trigramDict={}
+    uniCount=0; biCount=0
     with open(fl, 'r') as Data:
         lines = [l.split() for l in Data]
-        lines.insert(0,'<s>')
-        lines.append('</s>')
-        print(lines[1])
+        #lines.insert(0,['<s>'])
+        #lines.append(['</s>'])
+        #print(lines[1])
 
         #unigram
         for uline in range(len(lines)):
-            for word in lines[uline]:
-                  unigramList[word]={word}
-                  trainData.update({'unigram':unigramList})
-                  totalCount=totalCount+1
-        print(len(unigramList))
-        print(totalCount)
-        print(unigramList)
+            # count into dictionary
+            uni = [];
+            # check if its first line, middle lines or last line
+            if (uline == 0):
+                uni = ['<s>'] + lines[uline]
+            elif (uline == len(lines)-1):
+                uni = lines[uline] + ['</s>']
+            else:
+                uni = lines[uline]
+            for uword in range(len(uni)):
+                val = 1; current = uni[uword]
+                if (unigramDict.get((current))):
+                    val = unigramDict.get((current)) + 1
+                unigramDict.update({(current): val})
+        #print(uniCount)
+        #print(unigramDict)
+        print(len(unigramDict))
 
         #bigrams
         for bline in range (len(lines)):
-            d=lines[bline]
-            for bword in range(len(lines[bline])):
-                if (bword==len(lines[bline])-1):
-                    nword=bword
-                else:
-                    nword=bword+1
-                current=d[bword]
-                next=d[nword]
-                #print("-------",current)
-                trainData[(current, next)]=0
-        print(trainData)
+            bi=[];
+            bi=['<s>']+lines[bline]+['</s>']
+            for bword in range(len(bi)-1):
+                val = 1; nword=bword+1;
+                current=bi[bword]; next=bi[nword]
+                if(bigramDict.get((current,next))):
+                    val=bigramDict.get((current,next))+1
+                bigramDict.update({(current,next):val})
+       # print(bigramDict)
+        print(len(bigramDict))
 
+        # trigrams
+        for tline in range(len(lines)):
+            tri = [];
+            tri = ['<s>']+['<s>'] + lines[tline] + ['</s>']+['</s>']
+            for tword in range(len(tri) - 2):
+                val = 1; nword = tword + 1; nnword=tword+2;
+                current = tri[tword];  next = tri[nword]; nnext=tri[nnword]
+                if (trigramDict.get((current, next, nnext))):
+                    val = trigramDict.get((current, next, nnext)) + 1
+                trigramDict.update({(current, next, nnext): val})
+       # print(trigramDict)
+        print(len(trigramDict))
 
-readData("minCorpus.txt");
+def processProbs():
+    print("hello")
+
+processGrams("minCorpus.txt");
